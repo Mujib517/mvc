20c8ed19c625b50243f09620c47c8d0529f5a16c
Mujib517 <Mujib517@gmail.com>
2018-09-04 11:54:07 +0530

    Ticket Svc

--- MedMvc.Core/Db/AppContext.cs
+++ MedMvc.Core/Db/AppContext.cs
@@ -9,5 +9,6 @@ namespace MedMvc.Core.Db
         public DbSet<Campaign> Campaigns { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Address> Addresses { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
     }
 }
---
+++ MedMvc.Core/Services/TicketService.cs
@@ -0,0 +1,22 @@
+﻿
+using MedMvc.Core.Db;
+using MedMvc.Core.Entities;
+
+namespace MedMvc.Core.Services
+{
+    public class TicketService
+    {
+        private AppContext _db;
+
+        public TicketService()
+        {
+            _db = new AppContext();
+        }
+
+        public void Add(Ticket ticket)
+        {
+            _db.Tickets.Add(ticket);
+            _db.SaveChanges();
+        }
+    }
+}
